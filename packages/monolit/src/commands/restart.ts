23eeff4b9fe3d17708b46bf4cde3d285e8028088
Oliver Salzburg <oliver.salzburg@gmail.com>
2020-12-18 20:31:02 +0100

    feat: Launch PLT in restart

--- packages/monolit/src/commands/restart.ts
+++ packages/monolit/src/commands/restart.ts
@@ -3,6 +3,9 @@ import { CwdParser } from "../CwdParser";
 import { getExtensionInstance } from "../extension";
 import { Log } from "../Log";
 
+/**
+ * The command should restart the current configuration.
+*/
 export async function restart(context: vscode.ExtensionContext) {
   const extensionInstance = getExtensionInstance();
 
@@ -14,6 +17,21 @@ export async function restart(context: vscode.ExtensionContext) {
   vscode.debug.stopDebugging();
 
   const selectedCwd = CwdParser.cwdFromVariant(extensionInstance.activeSession);
+
+  const tasks = await extensionInstance.taskCache;
+
+  const userDefinedPreLaunchTask = extensionInstance.activeConfiguration.configuration.preLaunchTask;
+  if (userDefinedPreLaunchTask) {
+    const plt = tasks.find(task => task.name === userDefinedPreLaunchTask);
+
+    if (plt) {
+      await getExtensionInstance().executeTask(plt, selectedCwd);
+    } else {
+      Log.warn(`  ? ${userDefinedPreLaunchTask} could not be found.`);
+    }
+  } else {
+    Log.debug(`  - no preLaunchTask requested.`);
+  }
 
   return extensionInstance.activeConfiguration.launch(selectedCwd);
 }
