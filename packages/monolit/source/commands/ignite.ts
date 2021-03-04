3036b2f25639df7430f143a45d87cce03e32c494
Oliver Salzburg <oliver.salzburg@gmail.com>
2021-03-04 22:36:40 +0100

    feat: Experimental easy entrypoint

---
+++ packages/monolit/source/commands/ignite.ts
@@ -0,0 +1,55 @@
+import * as vscode from "vscode";
+import { Log } from "../Log";
+import { restart } from "./restart";
+import { start } from "./start";
+
+/**
+ * The command should let the user pick an action that makes sense at any state.
+ */
+export async function ignite(context: vscode.ExtensionContext) {
+  if (!Array.isArray(vscode.workspace.workspaceFolders)) {
+    Log.warn("No workspace open. Aborting.");
+    return;
+  }
+
+  if (vscode.debug.activeDebugSession) {
+    const selection = await vscode.window.showQuickPick(
+      [
+        {
+          id: "START_ANOTER",
+          label: "Start another session.",
+          detail: "The current session(s) will keep running and we'll start a new one.",
+        },
+        {
+          id: "REPLACE_CURRENT",
+          label: "Replace current session.",
+          detail: "Exit current session and start a new one.",
+        },
+        {
+          id: "RESTART_CURRENT",
+          label: "Actually, restart current session.",
+          detail: "Restart the current session.",
+        },
+      ],
+      {
+        placeHolder: "You're already debugging. What do you want to do?",
+      }
+    );
+
+    if (!selection) {
+      // Action cancelled
+      return;
+    }
+
+    if (selection.id === "START_ANOTHER") {
+      return start(context);
+    } else if (selection.id === "REPLACE_CURRENT") {
+      await vscode.debug.stopDebugging();
+      return start(context);
+    } else if (selection.id === "RESTART_CURRENT") {
+      return restart(context);
+    }
+  } else {
+    return start(context);
+  }
+}
