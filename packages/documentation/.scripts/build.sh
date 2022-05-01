fb7868ce71c3237e6e53d7658d748ab4a25143bf
Oliver Salzburg <oliver.salzburg@gmail.com>
2022-05-01 16:32:03 +0200

    docs: Use published container image to build

--- packages/documentation/.scripts/build.sh
+++ packages/documentation/.scripts/build.sh
@@ -3,8 +3,6 @@
 self=$(readlink -f "$0")
 basedir=$(dirname "$self")
 
-cd ${basedir}/..
-docker build -t mkdocs-material .
+cd ${basedir}/../../..
 
-cd ../..
-docker run --rm -it -v ${PWD}:/docs ghcr.io/oliversalzburg/mkdocs-material-ex:main build --config-file packages/documentation/mkdocs.yml --site-dir public
+docker run --rm -it -p 8000:8000 -v ${PWD}:/docs ghcr.io/oliversalzburg/mkdocs-material-ex:main build --config-file packages/documentation/mkdocs.yml --site-dir=public
